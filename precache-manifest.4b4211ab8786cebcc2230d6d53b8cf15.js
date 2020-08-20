f3a849b91f35ebb0a637285539df320c9c94fb7d
krsamir <samir1741012321@gmail.com>
2020-08-21 00:58:32 +0530

    Updates

---
+++ precache-manifest.4b4211ab8786cebcc2230d6d53b8cf15.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3344ef35efbae4c77f4c5339d7e35da9",
+    "url": "/Profile-Analyser/index.html"
+  },
+  {
+    "revision": "59a814be07e5fcf29373",
+    "url": "/Profile-Analyser/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9d3fc1c8b248d3d1fca6",
+    "url": "/Profile-Analyser/static/css/main.ec1274ae.chunk.css"
+  },
+  {
+    "revision": "59a814be07e5fcf29373",
+    "url": "/Profile-Analyser/static/js/2.0be0c83f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Profile-Analyser/static/js/2.0be0c83f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d3fc1c8b248d3d1fca6",
+    "url": "/Profile-Analyser/static/js/main.aeaf1a28.chunk.js"
+  },
+  {
+    "revision": "d87b97500581110f0a23",
+    "url": "/Profile-Analyser/static/js/runtime-main.016b067b.js"
+  },
+  {
+    "revision": "4a451dbc87cf94a5d124d5923e3056fb",
+    "url": "/Profile-Analyser/static/media/Loading.4a451dbc.jpeg"
+  }
+]);
